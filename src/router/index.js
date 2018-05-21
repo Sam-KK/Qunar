90d5cea97d944a1c5e50ea708adea3aa5529fc37
xianghong.yan <xianghong.yan@baozun.com>
2018-05-21 10:34:36 +0800

    '路由更改'

--- src/router/index.js
+++ src/router/index.js
@@ -5,17 +5,21 @@ import city from '@/components/city/city'
 
 Vue.use(Router)
 
-export default new Router({
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: home
-        },
-        {
-            path: '/city',
-            name: 'city',
-            component: city
-        }
-    ]
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: home
+    },
+    {
+        path: '/city',
+        name: 'city',
+        component: city
+    }
+]
+
+const router = new Router({
+    routes
 })
+
+export default router
